8663b82c881e231c58a0e482526f05e6e7bf4f5a
Ahmad Diffa <147916548+Diffajl@users.noreply.github.com>
2024-07-24 19:13:15 +0700

    Create 3.dart

---
+++ 3.dart
@@ -0,0 +1,14 @@
+void main() {
+  var name = 'ahmad' ' diffa ' 'jamaludin ';
+  var firstName = 'bambang';
+  var lastName = 'dudung';
+  var fullName = firstName + lastName;
+  var longString = '''
+ini adalah long string
+  ''';
+  print(name);
+  print(firstName);
+  print(lastName);
+  print(fullName);
+  print(longString);
+}
